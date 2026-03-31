64474b2e0796c22614dc5535c317a21c4ca63430
Jerry <gerardoa.guzmanh@gmail.com>
2026-03-31 03:04:50 -0600

    Add Resume component to main application layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Services from './components/services/Services';
+import Resume from './components/resume/Resume';
 import Portfolio from './components/portfolio/Portfolio';
 import Blog from './components/blog/Blog';
 import useLocalStorage from 'use-local-storage'
@@ -25,6 +26,7 @@ function App() {
                 <Home />
                 <About />
                 <Services />
+                <Resume />
                 <Portfolio />
                 <Blog />
             </main>
